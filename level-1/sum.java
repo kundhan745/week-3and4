62cab17a89115b74a67d1b7843c0b55f56e7a4b2
kundhan745 <kundhan143kumar@gmail.com>
2026-03-25 13:45:02 +0530

    sum

---
+++ level-1/sum.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class sum {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        double total = 0.0;
+        double number;
+
+        System.out.print("Enter a number (0 to stop): ");
+        number = sc.nextDouble();
+
+        while (number != 0) {
+            total = total + number;
+
+            System.out.print("Enter a number (0 to stop): ");
+            number = sc.nextDouble();
+        }
+
+        System.out.println("The total sum is " + total);
+
+        sc.close();
+    }
+}
