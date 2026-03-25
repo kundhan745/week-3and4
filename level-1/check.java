56ab4afdf51f1021026ce3ea217a0fdec9d7f3ca
kundhan745 <kundhan143kumar@gmail.com>
2026-03-25 13:33:59 +0530

    to find the num is positive or negative or zero

---
+++ level-1/check.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class check {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int number;
+        System.out.print("Enter a number: ");
+        number = sc.nextInt();
+
+        if (number > 0) {
+            System.out.println("positive");
+        } 
+        else if (number < 0) {
+            System.out.println("negative");
+        } 
+        else {
+            System.out.println("zero");
+        }
+
+        sc.close();
+    }
+}
