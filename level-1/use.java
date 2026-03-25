4b3c3795af1a99429c61435f52725209d97f6c84
kundhan745 <kundhan143kumar@gmail.com>
2026-03-25 13:54:04 +0530

    sum of numbers

---
+++ level-1/use.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class use {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n;
+        System.out.print("Enter a number: ");
+        n = sc.nextInt();
+
+        if (n > 0) {
+            int sumFor = 0;
+
+            for (int i = 1; i <= n; i++) {
+                sumFor = sumFor + i;
+            }
+
+            int sumFormula = n * (n + 1) / 2;
+
+            System.out.println("Sum using for loop = " + sumFor);
+            System.out.println("Sum using formula = " + sumFormula);
+
+            if (sumFor == sumFormula) {
+                System.out.println("Both computations are correct.");
+            } else {
+                System.out.println("The results are not the same.");
+            }
+
+        } else {
+            System.out.println("The number " + n + " is not a natural number");
+        }
+
+        sc.close();
+    }
+}
