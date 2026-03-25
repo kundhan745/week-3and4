a06c62dad014db898063a94203d7e985df8618d0
kundhan745 <kundhan143kumar@gmail.com>
2026-03-25 14:07:00 +0530

    multiplication done

---
+++ level-2/multi.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class multi {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int number;
+        System.out.print("Enter a number: ");
+        number = sc.nextInt();
+
+        for (int i = 6; i <= 9; i++) {
+            System.out.println(number + " * " + i + " = " + (number * i));
+        }
+
+        sc.close();
+    }
+}
