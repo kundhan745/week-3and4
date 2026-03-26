e4588733d670a2eaa81c7197914c233e2b7f03e8
kundhan745 <kundhan143kumar@gmail.com>
2026-03-26 14:53:57 +0530

    greatest number found

---
+++ level-2/great.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class great {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int number;
+        System.out.print("Enter a number: ");
+        number = sc.nextInt();
+
+        int greatestFactor = 1;
+
+        for (int i = number - 1; i >= 1; i--) {
+            if (number % i == 0) {
+                greatestFactor = i;
+                break;
+            }
+        }
+
+        System.out.println("The greatest factor of " + number + " (besides itself) is " + greatestFactor);
+
+        sc.close();
+    }
+}
