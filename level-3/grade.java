ffa58316fc34c425da807bbeebd67cebf36b7c6d
kundhan745 <kundhan143kumar@gmail.com>
2026-04-02 13:01:30 +0530

    to show the grade and avg marks of a student

---
+++ level-3/grade.java
@@ -0,0 +1,54 @@
+import java.util.Scanner;
+
+public class grade {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        double physics, chemistry, maths, average;
+        char grade;
+        String remarks;
+
+        System.out.print("Enter Physics marks: ");
+        physics = sc.nextDouble();
+
+        System.out.print("Enter Chemistry marks: ");
+        chemistry = sc.nextDouble();
+
+        System.out.print("Enter Maths marks: ");
+        maths = sc.nextDouble();
+
+        average = (physics + chemistry + maths) / 3;
+
+        if (average >= 80) {
+            grade = 'A';
+            remarks = "Level 4, above agency-normalized standards";
+        }
+        else if (average >= 70) {
+            grade = 'B';
+            remarks = "Level 3, at agency-normalized standards";
+        }
+        else if (average >= 60) {
+            grade = 'C';
+            remarks = "Level 2, below but approaching agency-normalized standards";
+        }
+        else if (average >= 50) {
+            grade = 'D';
+            remarks = "Level 1, well below agency-normalized standards";
+        }
+        else if (average >= 40) {
+            grade = 'E';
+            remarks = "Level 1-, too below agency-normalized standards";
+        }
+        else {
+            grade = 'R';
+            remarks = "Remedial standards";
+        }
+
+        System.out.println("\nAverage Marks: " + average);
+        System.out.println("Grade: " + grade);
+        System.out.println("Remarks: " + remarks);
+
+        sc.close();
+    }
+}
