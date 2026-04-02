82309701a0f199f4be5f1e426e3a2bcc35598614
kundhan745 <kundhan143kumar@gmail.com>
2026-04-02 12:55:28 +0530

    ti find leap year

---
+++ level-3/year.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class year {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a year: ");
+        int year = sc.nextInt();
+
+        if (year < 1582) {
+            System.out.println("Year should be >= 1582 (Gregorian Calendar)");
+        } 
+        else {
+            if (year % 400 == 0) {
+                System.out.println("Leap Year");
+            } 
+            else if (year % 100 == 0) {
+                System.out.println("Not a Leap Year");
+            } 
+            else if (year % 4 == 0) {
+                System.out.println("Leap Year");
+            } 
+            else {
+                System.out.println("Not a Leap Year");
+            }
+        }
+
+        sc.close();
+    }
+}
